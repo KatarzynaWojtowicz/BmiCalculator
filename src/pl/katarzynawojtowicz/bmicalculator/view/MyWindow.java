ab842786bf0e7bde6bf90ab1f611aba465160b17
Katarzyna Wojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2017-12-20 00:30:11 +0100

    Added screenshot. Changed view.

--- src/pl/katarzynawojtowicz/bmicalculator/view/MyWindow.java
+++ src/pl/katarzynawojtowicz/bmicalculator/view/MyWindow.java
@@ -1,10 +1,12 @@
 package pl.katarzynawojtowicz.bmicalculator.view;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -22,27 +24,40 @@ public class MyWindow extends JFrame implements ActionListener  {
 		super("BMI Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(1200, 500);
-		setSize(350, 300);
+		setSize(200, 250);
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
+		panel.setBackground(new Color(200, 200, 200));
 	
 		
+		JLabel heightLabel = new JLabel("Height:");
+		heightLabel.setBounds(20, 20, 100, 25);
+		panel.add(heightLabel);
+		
 		height = new JTextField(5);
-		height.setBounds(50, 50, 100, 25);
+		height.setBounds(70, 20, 100, 25);
 		panel.add(height);
 		
+		JLabel weightLabel = new JLabel("Weight:");
+		weightLabel.setBounds(20, 60, 100, 25);
+		panel.add(weightLabel);
+		
 		weight = new JTextField(5);
-		weight.setBounds(200, 50, 100, 25);
+		weight.setBounds(70, 60, 100, 25);
 		panel.add(weight);
 		
 		button = new JButton("Calculate");
-		button.setBounds(125, 100, 100, 25);
+		button.setBounds(70, 100, 100, 25);
 		button.addActionListener(this);
 		panel.add(button);
 		
+		JLabel resultLabel = new JLabel("BMI:");
+		resultLabel.setBounds(20, 140, 100, 25);
+		panel.add(resultLabel);
+		
 		result = new JTextField(5);
-		result.setBounds(125, 150, 100, 25);
+		result.setBounds(70, 140, 100, 25);
 		panel.add(result);
 		result.setEditable(false);
 		result.setEnabled(true);
